df397ff0b304f139c7b67c9749142463d71348cb
Dungha125 <dungha122405@gmail.com>
2024-09-14 04:47:21 +0700

    dungha

--- src/component/sources/AddEvent.jsx
+++ src/component/sources/AddEvent.jsx
@@ -154,7 +154,7 @@ const AddEvent = ({ onAddEvent }) => {
                         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                     >
                         <option value="">Chọn học kỳ đang học</option>
-                        <option value="7">Học kỳ 1</option>
+                        <option value="1">Học kỳ 1</option>
                         <option value="9">Học kỳ 2</option>
                     </select>
                 </div>
