0ec250a996e95ce0dfa91bb36c20bb6243c20120
Dungha125 <dungha122405@gmail.com>
2024-10-11 19:53:48 +0700

    dungha

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
   return (
     <div className='w-full h-[100vh] flex flex-col items-center justify-center gap-4 p-[2rem] relative'>
           <div className='w-full bottom-0 right-0 absolute text-center text-xs mb-1'>
-      <span>Copyright@2024 Ver:2024.10.07 Đoàn thanh niên Học viện</span>
+      <span>Copyright@2024 Ver:2024.10.11 Đoàn thanh niên Học viện</span>
       <br></br>
       <span>Created by Liên chi Đoàn Khoa CNTT1-PTIT</span>
     </div>
